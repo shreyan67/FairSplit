4f169fd2af39e51e4d9b4203339f2799a5241213
Shreyan <shreyanacharjee21@gmail.com>
2025-07-14 09:23:49 +0530

    Fix: Use express.Router() in debts route file

--- server/routes/debts.js
+++ server/routes/debts.js
@@ -1,23 +1,23 @@
 const express = require("express");
+const router = express.Router();
 const debtController = require("../controllers/debt_controller");
-const app = express();
 
 // Get a list of all debts.
-app.get("/debts", debtController.getDebts);
+router.get("/debts", debtController.getDebts);
 
 // Get a list of all optimised debts.
-app.get("/optimisedDebts", debtController.getOptimisedDebts);
+router.get("/optimisedDebts", debtController.getOptimisedDebts);
 
 // Get a debt by lender and borrower.
-app.get("/debts/:from/:to", debtController.getDebtBetweenUsers);
+router.get("/debts/:from/:to", debtController.getDebtBetweenUsers);
 
 // Add a new debt between two users.
-app.post("/debts/add", debtController.addDebt);
+router.post("/debts/add", debtController.addDebt);
 
 // Settle a debt between two users.
-app.post("/debts/settle", debtController.settleDebt);
+router.post("/debts/settle", debtController.settleDebt);
 
 // Delete a debt between a lender and borrower.
-app.delete("/debts/:from/:to", debtController.deleteDebtBetweenUsers);
+router.delete("/debts/:from/:to", debtController.deleteDebtBetweenUsers);
 
-module.exports = app;
+module.exports = router;
